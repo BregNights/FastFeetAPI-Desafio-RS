37d2def3b0bcdbb900c7e0ce3e6fd427cf2cd6c7
BregNights <daniel_baumann567@hotmail.com>
2025-12-03 17:49:49 -0300

    add package presenter

---
+++ src/infra/http/presenters/package-presenter.ts
@@ -0,0 +1,16 @@
+import { Package } from "@/domain/carrier/enterprise/entities/package"
+
+export class PackagePresenter {
+  static toHTTP(pkg: Package) {
+    return {
+      id: pkg.id.toString(),
+      trackingCode: pkg.trackingCode,
+      description: pkg.description,
+      recipientId: pkg.recipientId.toString(),
+      courierId: pkg.courierId.toString(),
+      status: pkg.status,
+      createdAt: pkg.createdAt,
+      updatedAt: pkg.updatedAt,
+    }
+  }
+}
