f302bf6b49237114144c1d9e5dcd68759da01a16
BregNights <daniel_baumann567@hotmail.com>
2025-12-08 14:22:16 -0300

    update

--- src/domain/carrier/application/repositories/recipients-repository.ts
+++ src/domain/carrier/application/repositories/recipients-repository.ts
@@ -1,6 +1,6 @@
 import { Recipient } from "../../enterprise/entities/recipient"
 
-export interface FindManyNearbyParams {
+export interface FindManyNearbyCourierParams {
   latitude: number
   longitude: number
 }
@@ -9,8 +9,11 @@ export abstract class RecipientsRepository {
   abstract create(recipient: Recipient): Promise<void>
   abstract findById(id: string): Promise<Recipient | null>
   abstract findByCPF(cpf: string): Promise<Recipient | null>
-  abstract findManyNearby(params: FindManyNearbyParams): Promise<Recipient[]>
   abstract findManyRecipients(page: number): Promise<Recipient[]>
+  abstract findManyRecipientsNearbyCourier(
+    courierId: string,
+    params: FindManyNearbyCourierParams
+  ): Promise<Recipient[]>
   abstract save(recipient: Recipient): Promise<void>
   abstract delete(recipient: Recipient): Promise<void>
 }
