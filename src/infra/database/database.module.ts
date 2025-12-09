c047a921121ecf99abed486363ee12afd70c6ce1
BregNights <daniel_baumann567@hotmail.com>
2025-12-09 12:12:16 -0300

    add NotificationsRepository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,11 @@
 import { CouriersRepository } from "@/domain/carrier/application/repositories/couriers-repository"
 import { PackagesRepository } from "@/domain/carrier/application/repositories/packages-repository"
 import { RecipientsRepository } from "@/domain/carrier/application/repositories/recipients-repository"
+import { NotificationsRepository } from "@/domain/notification/application/repositories/notifications-repository"
 import { PrismaService } from "@/infra/database/prisma/prisma.service"
 import { Module } from "@nestjs/common"
 import { PrismaCouriersRepository } from "./prisma/repositories/prisma-couriers-repository"
+import { PrismaNotificationsRepository } from "./prisma/repositories/prisma-notifications-repository"
 import { PrismaPackagesRepository } from "./prisma/repositories/prisma-packages-repository"
 import { PrismaRecipientsRepository } from "./prisma/repositories/prisma-recipients-repository"
 
@@ -23,12 +25,17 @@ import { PrismaRecipientsRepository } from "./prisma/repositories/prisma-recipie
       provide: PackagesRepository,
       useClass: PrismaPackagesRepository,
     },
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
+    },
   ],
   exports: [
     PrismaService,
     CouriersRepository,
     RecipientsRepository,
     PackagesRepository,
+    NotificationsRepository,
   ],
 })
 export class DatabaseModule {}
